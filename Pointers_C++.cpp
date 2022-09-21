f6c1f37fdb6dd3a04937a5ba4a802e70d6878b66
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2022-09-21 15:44:08 -0300

    Update Pointers_C++.cpp

--- Pointers_C++.cpp
+++ Pointers_C++.cpp
@@ -15,35 +15,27 @@ using namespace std;
  * ********************************************************/
 
 int main(){
-
+	/**Vetor de Inteiros **/
 	int u[10];
 	int v[10];
 	int i;
+	/**Ponteiros para Inteiros**/
 	int *pu, *pv;
-
+	/**Obtendo o Endereço de **/
 	pu = &u[0];
-
 	pv = &v[0];
-
-	for(i = 0; i < 10; i++){ //  OR while(*pu)
-		
+	/**Laço for **/
+	for(i = 0; i < 10; i++){ //  OR while(*pu
 		*(pu+i) = 0;
-			cout << "Enter Values: ["<< i <<"]" << '\n';
-				cin >> i;
-
-				*(pu+i) = i;
-				
-				/* *(pu) += 1; */
+		cout << "Enter Values: ["<< i <<"]" << '\n';
+		cin >> i;
+		*(pu+i) = i;
+		/* *(pu) += 1;  Ver outro Valores*/
 	}
-
 	for(int i : u){
-
-		/*Escreva os valores para o Usuário */
-
+		/*Escreva os endereços e os valores para o Usuário */
 		cout << pu+i << "->" << *(pu+i) << '\n' << '\n';
 	}
-
 	return 0;
-
 }
 
